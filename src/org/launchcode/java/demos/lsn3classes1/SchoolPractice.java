3deb2d0131d8807ed59f8816693490002cf77687
Shweta Shrivastava <shweta.shrivastav@gmail.com>
2023-01-11 17:02:20 -0600

    exercise for chapter4 completed

--- src/org/launchcode/java/demos/lsn3classes1/SchoolPractice.java
+++ src/org/launchcode/java/demos/lsn3classes1/SchoolPractice.java
@@ -3,5 +3,10 @@ package org.launchcode.java.demos.lsn3classes1;
 public class SchoolPractice {
     public static void main(String[] args) {
         // Instantiate your Student class for part 2 here!
+        Student newStudent = new Student();
+        newStudent.setName("Shweta");
+        newStudent.setGpa(4.0);
+        newStudent.setStudentId(123);
+        newStudent.setNumberOfCredits(1);
     }
 }
