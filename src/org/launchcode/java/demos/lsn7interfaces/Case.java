e8c52aea82c7f94f052d6c2e568054132f4e7a6c
Shweta Shrivastava <shweta.shrivastav@gmail.com>
2023-02-01 00:28:23 -0600

    Completed chapter 8 excercise

--- src/org/launchcode/java/demos/lsn7interfaces/Case.java
+++ src/org/launchcode/java/demos/lsn7interfaces/Case.java
@@ -7,6 +7,8 @@ public class Case {
     private ArrayList<Flavor> flavors = new ArrayList<>();
     private ArrayList<Cone> cones = new ArrayList<>();
 
+    private ArrayList<Topping> toppings = new ArrayList<>();
+
     public Case(){
         Cone cone1 = new Cone("Waffle", 1.25, new ArrayList<>(Arrays.asList( "gluten")));
         Cone cone2 = new Cone("Sugar", 0.75, new ArrayList<>(Arrays.asList( "gluten")));
